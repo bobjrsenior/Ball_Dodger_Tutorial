ef7111effdaeb26b027fa122a082360174b779cc
bobjrsenior <rcarpe31@uncc.edu>
2015-10-31 16:45:52 -0400

    Added a very simple main menu with just a game name and a play/quit button

---
+++ Assets/Scripts/Main_Menu_Controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu_Controller : MonoBehaviour {
+
+    public void play()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
