6b65357d15d812c2810848356987a1d4d501d300
bobjrsenior <rcarpe31@uncc.edu>
2015-10-31 13:53:24 -0400

    Added simple player movement bounded within the play window

---
+++ Assets/Scripts/Player_Controls.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controls : MonoBehaviour {
+
+    /// <summary>
+    /// How fast the player can move in units/second
+    /// </summary>
+    private float movementSpeed;
+
+    /// <summary>
+    /// Holds half of the Windows size in Unity Units for bounds checking
+    /// </summary>
+    public Vector2 halfWindowSize;
+
+    // Use this for initialization
+    void Start () {
+        movementSpeed = 5.0f;
+        print(((float)Screen.width) / Screen.height);
+        halfWindowSize.x = halfWindowSize.y * Screen.width / Screen.height;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime, 0);
+        if(Mathf.Abs(transform.position.x) > halfWindowSize.x)
+        {
+            transform.position = new Vector2(Mathf.Sign(transform.position.x) * halfWindowSize.x, transform.position.y);
+        }
+        if (Mathf.Abs(transform.position.y) > halfWindowSize.y)
+        {
+            transform.position = new Vector2(transform.position.x, Mathf.Sign(transform.position.y) * halfWindowSize.y);
+        }
+    }
+}
