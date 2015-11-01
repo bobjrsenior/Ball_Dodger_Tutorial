cf2b68e4bd665f6c764c57aee3461ba35092b161
bobjrsenior <rcarpe31@uncc.edu>
2015-11-01 11:42:48 -0500

    added high scores to the main menu and made the main menu UI scale which screen size

--- Assets/Scripts/Main_Menu_Controller.cs
+++ Assets/Scripts/Main_Menu_Controller.cs
@@ -1,7 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Main_Menu_Controller : MonoBehaviour {
+
+    /// <summary>
+    /// References the UI object that will display the players best wave on the main menu ui
+    /// </summary>
+    public Text bestWaveUI;
+
+    /// <summary>
+    /// References the UI object that will display the players best play time on the main menu ui
+    /// </summary>
+    public Text bestTimeUI;
+
+    void Start()
+    {
+        //Set up the highscores in the ui
+        bestWaveUI.text = "Best Wave: " + getHighestWave();
+        bestTimeUI.text = "Best Time: " + (int) (getHighestTime() * 100) / 100.0f;
+    }
+
+    /////UI Button Functions
 
     public void play()
     {
@@ -12,4 +32,17 @@ public class Main_Menu_Controller : MonoBehaviour {
     {
         Application.Quit();
     }
+
+    /////Stat Storage and Retreival
+
+    int getHighestWave()
+    {
+        return PlayerPrefs.GetInt("wave", 0);
+    }
+
+    float getHighestTime()
+    {
+        return PlayerPrefs.GetFloat("time", 0);
+    }
+
 }
